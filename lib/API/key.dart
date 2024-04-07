0ff6de23bda502e7e2982ae2bec331cf5b65996f
Muhammad Hamad <113880440+Muhammadhamad1@users.noreply.github.com>
2024-04-07 12:57:35 +0500

    key.dart

--- lib/API/key.dart
+++ lib/API/key.dart
@@ -1,3 +1,3 @@
 class ApiKey{
-static const  myUrl = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyCzrPnB5n55BQyQ1QwyXT5DEE8a2UAAI_Y';
+static const  myUrl = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=YOUR_API_KEY';
 }
